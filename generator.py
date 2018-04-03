8fae7aa0378b582f964b8470e5eac5932ebb9315
beanandbean <wangsw.a@gmail.com>
2018-04-03 21:26:41 +0800

    minor

--- generator.py
+++ generator.py
@@ -205,7 +205,7 @@ class Board:
         d = self.__dict__
         keys = sorted(d.keys())
         for key in keys:
-            if key == "content" or key == "award_area":
+            if key in ["award", "award_area", "award_index", "content"]:
                 print(" " * 4 + key + ":", file = file)
                 for line in d[key]:
                     print(" " * 6 + str(line), file = file)
