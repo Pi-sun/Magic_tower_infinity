a1a79d415d6c56dd68e6d5117c0ce8c11693ed62
Pi-sun <akaksd@163.com>
2018-04-16 23:26:44 +0800

    mapping

--- map_generate.py
+++ map_generate.py
@@ -3,7 +3,8 @@ import generator
 
 def to_real_map(section,section_index):
     monsters=monster_for(section_index)
-
+    big_location=[]
+    small_location=[]
     for i in range(len(section.size)):
         section.floors[i].map=list()
         for j in range(section.floors[i].size):
@@ -33,7 +34,24 @@ def to_real_map(section,section_index):
                     new_map[ci][ri]='hidden door'
                 # begin to generate award
 
-                if 
+                if section.floors[i].difficulty[ci][ri]==1:
+                    small_location.append([i,ci,ri])
+                    luck=random.randint(1,section.small_award)
+                    if luck <= section.yellow_door*1.05:
+                        new_map[ci][ri]='yellow key'
+                        section.yellow_key+=1
+                    else:
+                        new_map[ci][ri]='small flask'
+                        
+                if section.floors[i].difficulty[ci][ri]==5:
+                    luck=random.randint(1,section.big_award)
+                    big_location.append([i,ci,ri])
+                    if luck <= section.blue_door:
+                        new_map[ci][ri]='blue key'
+                    else:
+                        new_map[ci][ri]='big flask'
+                        
+                    
                 # begin to generate monsters
 
 
@@ -56,4 +74,26 @@ def to_real_map(section,section_index):
                     monster_chosen=random.randint(0,len(choices))
 
                     new_map[ci][ri]='level'+str(section_index)+ 'monster'+ str(monster_chosen)
+
+            # award adjustment
+    while section.blue_gem<11:
+        for i in big_location:
+            luck=random.randint(1,section.big_award)
+            if luck<5:
+                section.floors[i[0]].map[i[1]][i[2]]='blue gem'
+                section.blue_gem+=1
+                
+    while section.red_gem<11:
+        for i in big_location:
+            luck=random.randint(1,section.big_award)
+            if luck<5:
+                section.floors[i[0]].map[i[1]][i[2]]='red gem'
+                section.blue_gem+=1
+
+    while section.yellow_key<0.9*section.yellow_door:
+        for i in small_location:
+            luck=random.randint(1,section.big_award)
+            if luck<5:
+                section.floors[i[0]].map[i[1]][i[2]]='yellow key'
+                section.yellow_key+=1
 
